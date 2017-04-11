3d2cb1f4f2f07709fa69200525ffd7ac58ef9d7c
= <=>
2017-04-11 09:47:44 +0300

    addex tests and proxy

--- nginx/run_tests.sh
+++ nginx/run_tests.sh
@@ -3,6 +3,9 @@
 rm -rf reports
 mkdir reports
 
-ab -c 10 -n 10000 http://localhost:80/ > reports/gunicorn_wsgi.txt
-ab -c 10 -n 10000 http://localhost:8081/ > reports/nginx_gunicorn_wsgi.txt
+#with proxy cache
+ab -c 10 -n 10000 http://localhost:80/ > reports/proxy.txt
+#without proxy cache
+ab -c 10 -n 10000 http://localhost:8081/ > reports/noproxy.txt
+#static
 ab -c 10 -n 10000 http://localhost/static/ > reports/nginx_static.txt
