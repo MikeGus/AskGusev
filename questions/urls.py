958042ca4fc70aff99d7c0cdb7fc361b434231df
= <=>
2017-04-11 18:14:53 +0300

    added param

--- questions/urls.py
+++ questions/urls.py
@@ -4,6 +4,6 @@ from questions.views import AboutView
 from questions.views import get_post
 
 urlpatterns = [
-    url(r'^about$', AboutView.as_view(), name='about'),
-    #url(r'^post$', get_post, name='getpost'),
+    url(r'^about', AboutView.as_view(), name='about'),
+    url(r'^post$', get_post, name='getpost'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -13,27 +13,12 @@ class AboutView(TemplateView):
     template_name = "about.html"
 
 
-def get_post(environ, start_response):
+def get_post(request):
+    data = []
+    if request.method == 'GET':
+        data = request.GET
+    elif request.method == 'POST':
+        data = request.POST
 
-    output = []
-
-    output.append('<form method="post">')
-    output.append('<input type="text" name = "data">')
-    output.append('<input type="submit" value="Send post data">')
-    output.append('</form>')
-
-    d = parse_qsl(environ['QUERY_STRING'])
-    if environ['REQUEST_METHOD'] == 'POST':
-        output.append(pformat(environ['wsgi.input'].read()))
-
-    if environ['REQUEST_METHOD'] == 'GET':
-        if environ['QUERY_STRING'] != '':
-            output.append('<label>')
-            for ch in d:
-                output.append(' = '.join(ch))
-            output.append('</label>')
-
-    output_len = sum(len(line) for line in output)
-    start_response('200 OK', [('Content-type', 'text/html'), ('Content-Length', str(output_len))])
-
-    return output
+    return render(request, 'about.html', {'method' : request.method,
+                                          'data' : data})
